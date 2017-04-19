f032183eae518a4c985339a99cd9f03a8314da08
Jason Tsai <tez125@g.ncu.edu.tw>
2017-04-20 00:04:45 +0800

    Initial Commit

---
+++ TMCore/TMCore.h
@@ -0,0 +1,19 @@
+//
+//  TMCore.h
+//  TMCore
+//
+//  Created by Jason Tsai on 2017/4/20.
+//  Copyright © 2017年 YomStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMCore.
+FOUNDATION_EXPORT double TMCoreVersionNumber;
+
+//! Project version string for TMCore.
+FOUNDATION_EXPORT const unsigned char TMCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMCore/PublicHeader.h>
+
+
